ba2def0e25a51f24f0d216e0ff05c61bf091267c
souko2525 <yusuke.h0808@gmail.com>
2017-09-27 00:38:23 +0900

    add calc file

---
+++ calc.go
@@ -0,0 +1,23 @@
+package calc
+
+import "fmt"
+
+type enemy struct {
+	Name string
+	HP   int
+	A    int
+	T    int
+	D    int
+}
+
+type enemy struct {
+	Name string
+	HP   int
+	A    int
+	T    int
+	D    int
+}
+
+func main() {
+	fmt.Println("vim-go")
+}
