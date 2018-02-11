9d8e5a8aa60d8ad328612aeadfba14b97896efdc
souko2525 <yusuke.h0808@gmail.com>
2018-02-11 21:04:57 +0900

    add multiple GET

--- main.go
+++ main.go
@@ -11,26 +11,7 @@ import (
 	//"net/http"
 )
 
-/*
-type Template struct {
-	templates *template.Template
-}
-
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
-
-func Hello(c echo.Context) error {
-	return c.Render(http.StatusOK, "layout.html", "echo server")
-}
-*/
-
 func main() {
-	/*
-		t := &Template{
-			templates: template.Must(template.ParseFiles("templates/layout.html", "templates/hello.html")),
-		}
-	*/
 	if err := database.Connect(); err != nil {
 		os.Exit(1)
 	}
@@ -41,12 +22,4 @@ func main() {
 	router.Router(e)
 	e.Start(":3000")
 
-	/*
-		e.Renderer = t
-		e.GET("/", func(c echo.Context) error {
-			return c.String(http.StatusOK, "index page")
-		})
-		e.GET("/hello", Hello)
-		e.Logger.Fatal(e.Start(":1323"))
-	*/
 }
--- models/database_table.go
+++ models/database_table.go
@@ -2,12 +2,12 @@ package models
 
 import (
 	//"fmt"
-	"reflect"
-	//"strconv"
-	//"strings"
 	"github.com/jinzhu/gorm"
 	"github.com/labstack/echo"
 	db "github.com/souko2525/KanCoreAPI/database"
+	"reflect"
+	"strconv"
+	//"strings"
 )
 
 type DatabaseTable interface {
@@ -31,11 +31,33 @@ func colums(t DatabaseTable) []string {
 	return cols
 }
 
-func Select(t DatabaseTable, id int64, cont echo.Context) error {
-	//cols := "`" + strings.Join(colums(t), "`, `") + "`"
+func Select(t DatabaseTable, cond map[string]interface{}, cont echo.Context) error {
 	s := db.GetSession()
-	//result := s.First(t, id)
-	result := s.Table(t.TableName()).Select(colums(t)).First(t, id)
+	stmt := s.Table(t.TableName()).Select(colums(t)).Where(cond)
+	if cont.QueryParam("offset") != "" {
+		if v, e := strconv.ParseUint(cont.QueryParam("offset"), 10, 64); e != nil {
+			return e
+		} else {
+			stmt.Offset(v)
+		}
+
+	}
+
+	if cont.QueryParam("limit") != "" {
+		if v, e := strconv.ParseUint(cont.QueryParam("limit"), 10, 64); e != nil {
+			return e
+		} else {
+			stmt.Limit(v)
+		}
+
+	}
+
+	if cont.QueryParam("order_by") != "" {
+		stmt.Order(cont.QueryParam("order_by"))
+	}
+
+	result := stmt.Find(t)
+
 	if err := result.Error; err != nil {
 		return err
 	} else if result.RecordNotFound() {
